b26c7086293af15bee63eff4ce7ac8a2966eb318
wndy <114026479+wundeeh@users.noreply.github.com>
2022-12-12 14:08:47 -0600

    add comments for clarification

--- public/scripts/composer-char-counter.js
+++ public/scripts/composer-char-counter.js
@@ -1,10 +1,13 @@
 $(document).ready(function() {
+  // Counts the amount of characters in the text field
   $("#tweet-text").on("input", function(element) {
     let charCount = $(this).val().length;
     let charsLeft = 140 - charCount;
 
+  // A variable assigned to the number at the bottom right that represents the amount of characters left 
   let charLimit = $(this).parents("form").children(".newTweetBottom").children(".counter");
 
+  // Turns the charLimit number red when going overboard
   charLimit.text(charsLeft);
   if (charsLeft < 0) {
     charLimit.css('color', 'red');
